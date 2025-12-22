7a7e99e9d6f0448bcef9b50b8d2216636446d1b8
Pranshul061204 <threjapranshul@gmail.com>
2025-12-22 11:13:14 +0530

    Practicing Circular Queue

--- Queue/Practice/Q3.cpp
+++ Queue/Practice/Q3.cpp
@@ -22,3 +22,55 @@ void enqueue(int value)
         queue[rear] = value;
     }
 }
+void dequeue()
+{
+    if (front == -1)
+    {
+        cout << "Queue is Empty" << endl;
+        return;
+    }
+    else
+    {
+        cout << "Dequeued: " << queue[front] << endl;
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else
+        {
+            front = (front + 1) % Size;
+        }
+    }
+}
+void display()
+{
+    if (front == -1)
+    {
+        cout << "Queue is Empty" << endl;
+        return;
+    }
+    cout << "Queue elements: ";
+    int i = front;
+    while (true)
+    {
+        cout << queue[i] << " ";
+        if (i == rear)
+            break;
+        i = (i + 1) % Size;
+    }
+    cout << endl;
+}
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
+    enqueue(40);
+    enqueue(50);
+    display();
+    return 0;
+}
--- Queue/Practice/Q5.cpp
+++ Queue/Practice/Q5.cpp
@@ -1,1 +1,89 @@
 // Implementation of Circular Queue using Linked List in C++
+#include <iostream>
+using namespace std;    
+struct Node
+{
+    int data;
+    Node *next;
+};
+class CircularQueue
+{
+private:
+    Node *front;
+    Node *rear;
+public:
+    CircularQueue()
+    {
+        front = NULL;
+        rear = NULL;
+    }
+    void enqueue(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        if (front == NULL)
+        {
+            front = newNode;
+            rear = newNode;
+            rear->next = front;
+        }
+        else
+        {
+            rear->next = newNode;
+            rear = newNode;
+            rear->next = front;
+        }
+    }
+    void dequeue()
+    {
+        if (front == NULL)
+        {
+            cout << "Queue is Empty" << endl;
+            return;
+        }
+        if (front == rear)
+        {
+            cout << "Dequeued: " << front->data << endl;
+            delete front;
+            front = NULL;
+            rear = NULL;
+        }
+        else
+        {
+            Node *temp = front;
+            cout << "Dequeued: " << front->data << endl;
+            front = front->next;
+            rear->next = front;
+            delete temp;
+        }
+    }
+    void display()
+    {
+        if (front == NULL)
+        {
+            cout << "Queue is Empty" << endl;
+            return;
+        }
+        cout << "Queue elements: ";
+        Node *temp = front;
+        do
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        } while (temp != front);
+        cout << endl;
+    }
+};
+int main()
+{
+    CircularQueue cq;
+    cq.enqueue(10);
+    cq.enqueue(20);
+    cq.enqueue(30);
+    cq.display();
+    cq.dequeue();
+    cq.display();
+    cq.enqueue(40);
+    cq.display();
+    return 0;
+}
