800fb3a49ac8d0046ac152bbad2302ac3a0c56e7
Pranshul061204 <threjapranshul@gmail.com>
2025-12-23 09:32:29 +0530

    Practicing

--- Sorting/SELECTION_SORT/Practice/SS.cpp
+++ Sorting/SELECTION_SORT/Practice/SS.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int arr[10];
+    int arr[10], min_index;
     int n, i, j, index;
     cout << "Enter the Number of Elements: ";
     cin >> n;
@@ -19,8 +19,20 @@ int main()
         cout << arr[i] << "  " << endl;
     }
 
-//    Selection-Sort Logic 
-
+    //    Selection-Sort Logic
+    for (int i = 0; i < n - 1; i++)
+    {
+        min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+                min_index = j;
+        }
+    }
+    if (min_index != i)
+    {
+        swap(arr[i], arr[min_index]);
+    }
 
     cout << "Elements After Sorting is : ";
     for (i = 0; i < n; i++)
