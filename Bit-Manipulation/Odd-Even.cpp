c9d671cf7529b166402d8cd041eec08768580d98
Pranshul <threjapranshul@gmail.com>
2026-02-27 21:12:41 +0530

    Bit Manipulation-2

---
+++ Bit-Manipulation/Odd-Even.cpp
@@ -0,0 +1,14 @@
+// check odd even with bit manipulation
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n & 1)
+        cout << n << " is odd.";
+    else
+        cout << n << " is even.";
+    return 0;
+}
